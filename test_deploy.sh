b22bfa65c5146d6381c4de8a1a06f2ae8ab79e1a
Zhang Pluto <Pluto.Zhang@nbai.io>
2024-01-23 01:30:19 -0500

    Update deploy file

--- test_deploy.sh
+++ test_deploy.sh
@@ -4,6 +4,31 @@
 SOURCE_DIR="/tmp/provider_test/"
 DEST_DIR="provider_test/"
 BAK_DIR="provider_test_bak/"
+ZIP_FILE="/tmp/dist_testnet.zip"
+EXTRACTED_DIR="/tmp/dist_testnet/"
+
+# 检查并解压zip文件
+if [ -f "$ZIP_FILE" ]; then
+    # 解压文件到指定目录
+    unzip -o "$ZIP_FILE" -d "/tmp/"
+    echo "Zip file extracted."
+
+    # 删除zip文件
+    rm "$ZIP_FILE"
+    echo "Zip file removed."
+
+    # 重命名解压后的文件夹
+    if [ -d "$EXTRACTED_DIR" ]; then
+        mv "$EXTRACTED_DIR" "$SOURCE_DIR"
+        echo "Extracted folder renamed to $SOURCE_DIR"
+    else
+        echo "Error: Extracted directory not found!"
+        exit 1
+    fi
+else
+    echo "Error: ZIP file $ZIP_FILE not found!"
+    exit 1
+fi
 
 # 删除备份目录
 if [ -d "$BAK_DIR" ]; then
